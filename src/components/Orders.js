df350d7437f42ace37b96f53ff683d80e0ce7e9d
Jahnavi3045 <jahnavi3045@gmail.com>
2024-07-26 12:21:39 +0530

    created element in orders page

--- src/components/Orders.js
+++ src/components/Orders.js
@@ -3,7 +3,29 @@ import React from 'react'
 const Orders = () => {
   return (
     <div>
-      orders
+        {/* <div class="card" style={{width:'350px'}}>
+          <img src='./../images/card1.png' class="card-img-top" alt="..." style={{width:'200px'}}/>
+          <div class="card-body">
+            <h5 class="card-title">tokyo talkies striped frock</h5>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <a href="#" class="btn btn-primary">Track your order</a>
+          </div>
+        </div> */}
+        <div class="card mb-3" style={{maxWidth: '540px',marginTop:'15px',marginLeft:'15px'}}>
+          <div class="row g-0">
+            <div class="col-md-4">
+              <img src="./../images/card1.png" class="img-fluid rounded-start" alt="..." />
+            </div>
+            <div class="col-md-8">
+              <div class="card-body">
+                <h5 class="card-title">tokyo talkies striped frock</h5>
+                <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                {/* <p class="card-text"><small class="text-body-secondary">Last updated 3 mins ago</small></p> */}
+                <a href="#" class="btn btn-primary">Track your order</a>
+              </div>
+            </div>
+          </div>
+        </div>
     </div>
   )
 }
